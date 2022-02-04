409d1339a158178bf0442b23b68fc75c0362a0c4
Ricardo Furbino <ricardofurbino@gmail.com>
2022-02-04 01:29:24 -0300

    Primeiro teste

---
+++ cypress/integration/meu_teste_end_to_end.js
@@ -0,0 +1,31 @@
+// meu_teste_end_to_end.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('Teste End-to-End', () => {
+   it('Meu Primeiro Teste', () => {
+       // Abre o site
+       cy.visit('http://localhost:5000/')
+       
+       // Verifica se existe o livro desejado
+       cy.get('[data-id=3]').should('contain.text', 'Design Patterns')
+       
+       // Calcula o frete
+       cy.get('[data-id=3]').within(() => {
+          cy.get('input').type('10000-000')
+          cy.contains('Calcular Frete').click().then
+          cy.wait(2000)
+       })
+       cy.get('.swal-text').contains('O frete é: R$')
+
+       // Fecha o pop-up com o preço do frete
+       cy.get('.swal-button').click()
+
+       cy.contains('Comprar').click()
+       cy.wait(2000)
+       cy.get('.swal-button--confirm').click()
+   })
+ })
